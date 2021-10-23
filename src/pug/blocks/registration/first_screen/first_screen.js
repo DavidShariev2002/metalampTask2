7c10edd443dcebf2789e050e0141b0caf7c302a6
DavidShariev <david.shariev.2002@gmail.com>
2021-10-23 21:13:37 +0300

    Pages: registration added

---
+++ src/pug/blocks/registration/first_screen/first_screen.js
@@ -0,0 +1,5 @@
+import './first_screen.scss';
+
+
+
+console.log("%c first_screen block registration script is working", "color: purple");
--- src/pug/pages/registration/registration.js
+++ src/pug/pages/registration/registration.js
@@ -1,4 +1,7 @@
 
 import './registration.scss';
 
+import '../../blocks/registration/first_screen/first_screen'
+
+
 console.warn('registration script is working')
